48ed86ffa54af857675a2e728133d6bef5546c16
Abdul Rahman <abdul.rahman@autodesk.com>
2022-08-31 19:01:39 +0530

    added sw

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging/sw";
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const firebaseConfig = {
+    apiKey: "AIzaSyB39xSxaHiR2YAlwi6Suc1Uek_6L6W-bpw",
+    authDomain: "test-firebase-web-push-bb5ad.firebaseapp.com",
+    projectId: "test-firebase-web-push-bb5ad",
+    storageBucket: "test-firebase-web-push-bb5ad.appspot.com",
+    messagingSenderId: "823079035007",
+    appId: "1:823079035007:web:b466e764e9e45b2f3b5f4a",
+    measurementId: "G-PQN7X9RTEQ"
+};
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = getMessaging(firebaseConfig);
+
+// Handle incoming messages. Called when:
+// - a message is received while the app has focus
+// - the user clicks on an app notification created by a service worker
+//   `messaging.onBackgroundMessage` handler.
+import { getMessaging, onMessage } from "firebase/messaging";
+
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+  // ...
+});
