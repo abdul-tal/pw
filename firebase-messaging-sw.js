80c44cad8691b819c2a4e2c3ebdc76a8e39dcfcb
Abdul Rahman <abdul.rahman@autodesk.com>
2022-08-31 19:05:31 +0530

    sw fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,14 +17,3 @@ const firebaseConfig = {
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = getMessaging(firebaseConfig);
-
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.onBackgroundMessage` handler.
-import { getMessaging, onMessage } from "firebase/messaging";
-
-onMessage(messaging, (payload) => {
-  console.log('Message received. ', payload);
-  // ...
-});
